e6274bf902baaf04508689e3450c0c5f53b1bc13
Toi0126 <53397326+Toi0126@users.noreply.github.com>
2020-12-16 21:31:44 +0900

    Update AutoFitTextureView.java

--- Application/src/main/java/com/example/android/camera2basic/AutoFitTextureView.java
+++ Application/src/main/java/com/example/android/camera2basic/AutoFitTextureView.java
@@ -17,7 +17,9 @@
 package com.example.android.camera2basic;
 
 import android.content.Context;
+import android.os.Handler;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.TextureView;
 
 /**
@@ -28,16 +30,23 @@ public class AutoFitTextureView extends TextureView {
     private int mRatioWidth = 0;
     private int mRatioHeight = 0;
 
+
     public AutoFitTextureView(Context context) {
         this(context, null);
+
+
     }
 
     public AutoFitTextureView(Context context, AttributeSet attrs) {
         this(context, attrs, 0);
+
+
     }
 
     public AutoFitTextureView(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+
+
     }
 
     /**
@@ -55,13 +64,24 @@ public class AutoFitTextureView extends TextureView {
         mRatioWidth = width;
         mRatioHeight = height;
         requestLayout();
+
+
+
+
+
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+
+
+
+
         int width = MeasureSpec.getSize(widthMeasureSpec);
         int height = MeasureSpec.getSize(heightMeasureSpec);
+
         if (0 == mRatioWidth || 0 == mRatioHeight) {
             setMeasuredDimension(width, height);
         } else {
@@ -71,6 +91,10 @@ public class AutoFitTextureView extends TextureView {
                 setMeasuredDimension(height * mRatioWidth / mRatioHeight, height);
             }
         }
+
+
+
+
     }
 
 }
